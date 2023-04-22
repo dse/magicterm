a1b4e415c930f81840fa58918c316c91cf55a759
Darren Embry <dse@webonastick.com>
2023-04-22 19:40:04 -0400

    simplified; comments

--- share/magicterm/magicterm.sh
+++ share/magicterm/magicterm.sh
@@ -1,161 +1,89 @@
-# -*- mode: sh; sh-shell: bash -*-
+# -*- mode: sh; sh-shell: bash; comment-column: 56; fill-column: 79 -*-
 
 DCS=$'\eP'
 CSI=$'\e['
 ST=$'\e\\'
 OSC=$'\e]'
-BEL=$'\a'                   # accepted as string terminal for OSC cmds
+BEL=$'\a'                                               # OSC cmds only
 ESC=$'\e'
-
-MT_ECHO () {
-    (( mt_verbose )) && echo "$@" >&2
-}
 
 has_true_color () {
     [[ -v _has_true_color ]] && [[ "$_has_true_color" != "" ]] && return $_has_true_color
-
     local REPLY
-    local SAVECOLOR
-
-    MT_ECHO "truecolor? #1"
-
-    echo -n "${DCS}\$qm${ST}" >/dev/tty
-    read -d$'\\' -s -t 0.25 SAVECOLOR </dev/tty
-    if [[ "${SAVECOLOR}" = "" ]] ; then
-        _has_true_color=1
-        MT_ECHO "no"
-        return 1
-    fi
-
-    MT_ECHO "truecolor? #2"
-
-    echo -n "${CSI}38;2;254;254;254m" >/dev/tty
-    echo -n "${DCS}\$qm${ST}" >/dev/tty # request status string SGR
+    echo -n "${CSI}38;2;254;254;254m" >/dev/tty         # set fg color
+    echo -n "${DCS}\$qm${ST}" >/dev/tty                 # request fg color
     read -d$'\\' -s -t 0.25 </dev/tty
     if [[ "${REPLY}" = *"254;254;254"* ]] || [[ "${REPLY}" = *"254:254:254"* ]]; then
-        echo -n "${SAVECOLOR}" >/dev/tty
-        _has_true_color=0
-        MT_ECHO "yes"
-        return 0
+        echo -n "${CSI}m" >/dev/tty                     # reset fg color
+        _has_true_color=0; return 0
     fi
-
-    MT_ECHO "truecolor? #3"
-
-    echo -n "${CSI}38:2:254:254:254m" >/dev/tty
-    echo -n "${DCS}\$qm${ST}" >/dev/tty # request status string SGR
+    echo -n "${CSI}38:2:254:254:254m" >/dev/tty         # set fg color
+    echo -n "${DCS}\$qm${ST}" >/dev/tty                 # request fg color
     read -d$'\\' -s -t 0.25 </dev/tty
     if [[ "${REPLY}" = *"254;254;254"* ]] || [[ "${REPLY}" = *"254:254:254"* ]]; then
-        echo -n "${SAVECOLOR}" >/dev/tty
-        _has_true_color=0
-        MT_ECHO "yes"
-        return 0
+        echo -n "${CSI}m" >/dev/tty                     # reset fg color
+        _has_true_color=0; return 0
     fi
-
-    echo -n "${SAVECOLOR}" >/dev/tty
-    _has_true_color=1
-    MT_ECHO "no"
-    return 1
+    echo -n "${CSI}m" >/dev/tty                         # reset fg color
+    _has_true_color=1; return 1
 }
 
 has_256_color () {
     [[ -v _has_256_color ]] && [[ "$_has_256_color" != "" ]] && return $_has_256_color
-
     local REPLY
-
-    MT_ECHO "256? #1"
-
     echo -n $'\e]4;255;?\a' >/dev/tty
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "${REPLY}" != "" ]] ; then
-        _has_256_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_256_color=0; return 0
     fi
-
-    MT_ECHO "256? #2"
-
     echo -n $'\e]4:255:?\a' >/dev/tty
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "${REPLY}" != "" ]] ; then
-        _has_256_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_256_color=0; return 0
     fi
-
-    _has_256_color=1
-    MT_ECHO "no"
     return 1
 }
 
 has_88_color () {
     [[ -v _has_88_color ]] && [[ "$_has_88_color" != "" ]] && return $_has_88_color
-
     local REPLY
-
-    MT_ECHO "88? #1"
-
     echo -n $'\e]4;87;?\a' >/dev/tty
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "$REPLY" != "" ]] ; then
-        _has_88_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_88_color=0; return 0
     fi
-
-    MT_ECHO "88? #2"
-
     echo -n $'\e]4:87:?\a' >/dev/tty
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "$REPLY" != "" ]] ; then
-        _has_88_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_88_color=0; return 0
     fi
-
-    _has_88_color=1
-    MT_ECHO "no"
-    return 1
+    _has_88_color=1; return 1
 }
 
 has_16_color () {
     [[ -v _has_16_color ]] && [[ "$_has_16_color" != "" ]] && return $_has_16_color
-
     local REPLY
-
-    MT_ECHO "16? #1"
-
     echo -n $'\e]4;15;?\a' >/dev/tty
-
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "$REPLY" != "" ]] ; then
-        _has_16_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_16_color=0; return 0
     fi
-
-    MT_ECHO "16? #2"
-
     echo -n $'\e]4:15:?\a' >/dev/tty
     read -d$'\a' -s -t 0.25 </dev/tty
     if [[ "$REPLY" != "" ]] ; then
-        _has_16_color=0
-        MT_ECHO "yes"
-        return 0
+        _has_16_color=0; return 0
     fi
-
-    _has_16_color=1
-    MT_ECHO "no"
-    return 1
+    _has_16_color=1; return 1
 }
 
 set_terminfo_has () {
     if type -p infocmp >/dev/null ; then
-        MT_ECHO "will use infocmp"
+        (( mt_verbose )) && echo "will use infocmp" >&2
         terminfo_has () {
             infocmp "${1:-$TERM}" >/dev/null 2>/dev/null
         }
     elif type -p toe >/dev/null ; then
-        MT_ECHO "will use toe -a"
+        (( mt_verbose )) && echo "will use toe -a" >&2
         terminfo_has () {
             toe -a | awk '{print $1}' | grep --quiet --fixed-strings --line-regexp --regexp="${1:-$TERM}"
         }
