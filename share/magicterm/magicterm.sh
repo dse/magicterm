485e04ec9ac41692458c5021395fe599efc8ad57
Darren Embry <dse@webonastick.com>
2023-04-25 00:14:23 -0400

    if term sets COLORTERM, keep it; add gnome-terminal support

--- share/magicterm/magicterm.sh
+++ share/magicterm/magicterm.sh
@@ -76,7 +76,7 @@ has_16_color () {
     _has_16_color=1; return 1
 }
 
-set_terminfo_has () {
+set_terminfo_detector () {
     if type -p infocmp >/dev/null ; then
         (( mt_verbose )) && echo "will use infocmp" >&2
         terminfo_has () {
@@ -94,10 +94,52 @@ set_terminfo_has () {
 }
 
 do_magicterm () {
-    local OPTARG
-    local OPTIND=1
-    local OPTERR=1
-    local OPTION
+    set_terminfo_detector || return 1
+
+    # If your terminal emulator sets COLORTERM, make sure TERM is something
+    # that supports truecolor and is in terminfo.
+    #
+    # TODO --- I mean, can we assume that the terminal emulator is setting TERM
+    # to something that will work too?
+    if [[ -v COLORTERM ]] && [[ "${COLORTERM}" != "" ]] ; then
+        >&2 echo "TERM=${TERM}; COLORTERM=${COLORTERM}"
+        case "${TERM}" in
+            *-direct)
+                return 0
+                ;;
+            *-256color)
+                local newterm="${TERM%-256color}-direct"
+                if terminfo_has "${newterm}" ; then
+                    export TERM="${newterm}"
+                    return 0
+                fi
+                ;;
+            *-88color)
+                local newterm="${TERM%-88color}-direct"
+                if terminfo_has "${newterm}" ; then
+                    export TERM="${newterm}"
+                    return 0
+                fi
+                ;;
+            xterm)
+                if terminfo_has xterm-direct ; then
+                    export TERM=xterm-direct
+                    return 0
+                fi
+                ;;
+            screen|mintty|tmux)
+                local newterm="${TERM}-direct"
+                if terminfo_has "${newterm}" ; then
+                    export TERM="${newterm}"
+                    return 0
+                fi
+                if terminfo_has xterm-direct ; then
+                    export TERM=xterm-direct
+                    return 0
+                fi
+                ;;
+        esac
+    fi
 
     local mt_verbose=0
     local _has_true_color=''
@@ -105,6 +147,11 @@ do_magicterm () {
     local _has_88_color=''
     local _has_16_color=''
     local mt_short_circuit=1
+
+    local OPTARG
+    local OPTIND=1
+    local OPTERR=1
+    local OPTION
 
     while getopts 'vhf' OPTION "${@}" ; do
         case "${OPTION}" in
@@ -119,8 +166,6 @@ do_magicterm () {
         esac
     done
     shift $((OPTIND - 1))
-
-    set_terminfo_has || return 1
 
     if (( mt_short_circuit )) ; then
         if [[ -v TERM_PROGRAM ]] && [[ "${TERM_PROGRAM}" = "Apple_Terminal" ]] ; then
@@ -140,6 +185,13 @@ do_magicterm () {
             return 0
         elif [[ -v STY ]] && [[ "$STY" != "" ]] ; then
             export TERM=screen-256color; unset COLORTERM
+            return 0
+        elif [[ -v GNOME_TERMINAL_SCREEN ]] || [[ -v GNOME_TERMINAL_SERVICE ]] ; then
+            if terminfo_has xterm-direct && has_true_color ; then
+                export TERM=xterm-direct COLORTERM=truecolor
+                return 0
+            fi
+            export TERM=xterm-256color; unset COLORTERM
             return 0
         fi
     fi
